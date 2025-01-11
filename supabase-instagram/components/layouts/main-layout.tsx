b64faf064314230db174954dc78b5466efa7dc60
1gyou1 <1gyou1@naver.com>
2025-01-11 21:23:49 +0900

    2025-01-11 인증구현 - 인증링크 방식 회원가입 구현

---
+++ supabase-instagram/components/layouts/main-layout.tsx
@@ -0,0 +1,9 @@
+import Sidebar from "components/sidebar";
+export default async function MainLayout({ children }) {
+  return (
+    <main className="w-full h-screen flex items-center justify-center">
+      <Sidebar />
+      {children}
+    </main>
+  );
+}
