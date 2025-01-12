7301ffb6dbc27b5981326796fa82264c973f019d
1gyou1 <1gyou1@naver.com>
2025-01-12 18:39:25 +0900

    2025-01-11 Supabase Realtime 소개 + 채팅목록 구현

--- supabase-instagram/utils/recoil/atoms.ts
+++ supabase-instagram/utils/recoil/atoms.ts
@@ -3,3 +3,12 @@ export const searchState = atom({
   key: "searchState",
   default: "",
 });
+
+export const selectedUserIdState = atom({
+  key: "selectedUserIdState",
+  default: null,
+});
+export const selectedUserIndexState = atom({
+  key: "selectedUserIndexState",
+  default: null,
+});
